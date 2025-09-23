0e7e9569320f430062038cbb0a5f62bd0127f194
Maxim <maxim.lyan@bk.ru>
2025-09-23 23:30:18 +0500

    passwords

---
+++ homework/validate.py
@@ -0,0 +1,26 @@
+import re
+
+passwords = [
+    "qwerty",
+    "Password123",
+    "admin",
+    "HelloWorld!",
+    "Qwe123!@#",
+]
+
+verifiedPassword = [] 
+for password in passwords: 
+    verifiedPassword.append(password)
+    if not re.search(r"[A-Z]",password):          
+        print("слабый")
+    elif not re.search(r"[0-9]",password):          
+        print("слабый")
+    elif not re.search(r"[!@#$%^&*]",password):          
+        print("слабый")
+    elif len(password) < 8:
+        print("слабый")
+    else:
+        print("надежный")
+
+    
+
